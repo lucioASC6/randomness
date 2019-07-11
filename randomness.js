5acb07dc01b274e334b566a9cc6db1164df2676d
lucioASC6 <dannyang0227@gmail.com>
2019-07-11 12:52:34 -0400

    updated

--- randomness.js
+++ randomness.js
@@ -48,13 +48,23 @@ let student1 = students[Math.floor(Math.random() * 7)]
 let student2 = students[Math.floor(Math.random() * 7)]
 function randomGroup() {
     console.log(student0);
-    if (student0 == student1) {
-        while (student0 != student1) {
+    if(student1 == student0) {
+        while(student1 != student0) {
             student1 = randomStudent();
+            console.log(student1);
         }
     }
     else {
         console.log(student1);
     }
+    // if(student2 == student0 || student2 == student1) {
+    //     while(student2 != student0 && student2 != student1) {
+    //         student2 = randomStudent();
+    //         console.log(student2);
+    //     }
+    // }
+    // else {
+    //     console.log(student2);
+    // }
 }
 randomGroup();
